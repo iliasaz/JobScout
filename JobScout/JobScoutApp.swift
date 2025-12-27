73f828d45e9e3fba6d630286740dbc50c1c6e9ef
Ilia Sazonov <ilia_saz@yahoo.com>
2025-12-26 16:25:34 -0800

    Initial Commit

---
+++ JobScout/JobScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobScoutApp.swift
+//  JobScout
+//
+//  Created by Ilia Sazonov on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct JobScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JobScoutTests/JobScoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  JobScoutTests.swift
+//  JobScoutTests
+//
+//  Created by Ilia Sazonov on 12/26/25.
+//
+
+import Testing
+@testable import JobScout
+
+struct JobScoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
